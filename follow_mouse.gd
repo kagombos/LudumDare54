3df0e2ad1be8bae95239f2eaf87438e831b94168
Kamil Gombos <gremmlynn@gmail.com>
2023-09-29 18:26:58 -0500

    working texture!

--- follow_mouse.gd
+++ follow_mouse.gd
@@ -1,10 +1,9 @@
 extends CharacterBody2D
 
-var speed
+@export var speed = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var speed = 100
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
