b8661af0194fbcc4597c93578a40364a172964b4
Kamil Gombos <gremmlynn@gmail.com>
2023-09-29 19:52:32 -0500

    Fading lines

--- trail_control.gd
+++ trail_control.gd
@@ -4,8 +4,16 @@ extends Node2D
 @export var lineWidth = 20
 @export var mouseButton = 1
 var drawing = false
-var pathVectors = []
 var line
+
+class FadingLine:
+	var opacity = 1
+	var line: Line2D
+	
+	func _init(line):
+		self.line = line
+
+var fadingLines = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,7 +21,12 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if fadingLines.size():
+		for fadingLine in fadingLines:
+			fadingLine.opacity = fadingLine.opacity - 0.05
+			#if fadingLine.opacity <= 0:
+		#fadingLines.filter((line) => line.opacity <= 0)
+		self.queue_redraw()
 
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == mouseButton:
@@ -21,17 +34,20 @@ func _input(event):
 		if drawing:
 			line = Line2D.new()
 			line.add_point(event.position)
-			pathVectors.append(event.position)
 		else:
 			# end drawing, create path
-			pathVectors = []
+			fadingLines.append(FadingLine.new(line.duplicate()))
 			line.clear_points()
 			self.queue_redraw()
-	if event is InputEventMouseMotion and drawing:
+	if event is InputEventMouseMotion and drawing and event.position.distance_to(line.points[-1]) > 30:
 		line.add_point(event.position)
+		if line.points.size() > 30:
+			line.remove_point(0)
 		self.queue_redraw()
 		
 
 func _draw():
-	if line:
+	if line and line.points.size() > 2:
 		draw_polyline(line.points, lineColor, lineWidth)
+	for fadingLine in fadingLines:
+		draw_polyline(fadingLine.line.points, Color(lineColor.r, lineColor.g, lineColor.b, fadingLine.opacity), lineWidth)
