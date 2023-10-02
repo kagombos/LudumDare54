cf930e2acc867f2795e5f205118bafff040b4af9
Kamil Gombos <gremmlynn@gmail.com>
2023-10-02 15:30:11 -0500

    pause menu hooked up

--- UI/Scripts/PauseController.gd
+++ UI/Scripts/PauseController.gd
@@ -13,19 +13,28 @@ func _ready():
 func _input(event):
 	if event.is_action_pressed("Pause_Menu"):
 		if currentlyPaused:
-			get_tree().paused = false
-			currentlyPaused = false
-			Input.set_custom_mouse_cursor(gameCursor)
-			$PauseMenu.visible = false
-			get_viewport().warp_mouse(mousePos)
+			unpause()
 		else:
-			get_tree().paused = true
-			currentlyPaused = true
-			Input.set_custom_mouse_cursor(cursor)
-			$PauseMenu.visible = true
-			mousePos = get_viewport().get_mouse_position()
-			
+			pause()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func pause():
+	get_tree().paused = true
+	currentlyPaused = true
+	Input.set_custom_mouse_cursor(cursor)
+	$PauseMenu.visible = true
+	mousePos = get_viewport().get_mouse_position()
+
+func unpause():
+	get_tree().paused = false
+	currentlyPaused = false
+	Input.set_custom_mouse_cursor(gameCursor)
+	$PauseMenu.visible = false
+	get_viewport().warp_mouse(mousePos)
+
+func quit_to_menu():
+	unpause()
+	get_tree().change_scene_to_file("res://Main Menu.tscn")
+	MusicPlayer.get_node("GameMusic").stop()
+
+func quit_to_desktop():
+	get_tree().quit()
