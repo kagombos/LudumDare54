77028c134eca074dddfabd714b349461023627d9
Ian Kirk <iankirk@live.com>
2023-10-02 13:28:15 +1100

    Upgrades Spawn Cooldown

--- Player/Scripts/Air_Upgrade.gd
+++ Player/Scripts/Air_Upgrade.gd
@@ -13,12 +13,13 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var bodies = get_overlapping_bodies()
-	bodies = bodies.filter(func(body): return body.get_groups().has("Player"))
-	if bodies.size() > 0:
-		for i in bodies:
-			if statChoice == 0:
-				i.get_node("Weapon_Air").power *= 1.5
-			if statChoice == 1:
-				i.get_node("Weapon_Air").pierce += 0.1
-		owner.queue_free()
+	if not owner.spawning:
+		var bodies = get_overlapping_bodies()
+		bodies = bodies.filter(func(body): return body.get_groups().has("Player"))
+		if bodies.size() > 0:
+			for i in bodies:
+				if statChoice == 0:
+					i.get_node("Weapon_Air").power *= 1.5
+				if statChoice == 1:
+					i.get_node("Weapon_Air").pierce += 0.1
+			owner.queue_free()
