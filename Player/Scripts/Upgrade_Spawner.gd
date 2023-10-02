5d96b482f20700afa2a2d06fe4f7cfb1fcc0b193
Kamil Gombos <gremmlynn@gmail.com>
2023-10-01 20:27:22 -0500

    fix upgrade pos

--- Player/Scripts/Upgrade_Spawner.gd
+++ Player/Scripts/Upgrade_Spawner.gd
@@ -4,5 +4,5 @@ var upgrade_prefab = preload("res://Player/Prefabs/upgrades_parent.tscn")
 
 func level_up():
 	var upgrades = upgrade_prefab.instantiate()
-	upgrades.position = position
+	upgrades.position = owner.position
 	owner.add_sibling(upgrades)
