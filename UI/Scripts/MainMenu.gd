6e0fc1387eaebf7bb03d9f577a084208696b6c01
Ian Kirk <iankirk@live.com>
2023-10-02 13:06:53 +1100

    Menu Cursor

--- UI/Scripts/MainMenu.gd
+++ UI/Scripts/MainMenu.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
 
+func _ready():
+	Input.set_custom_mouse_cursor(preload("res://UI/Sprites/cursor.png"))
+
 func start_game():
 	get_tree().change_scene_to_file("res://arena.tscn")
 	
