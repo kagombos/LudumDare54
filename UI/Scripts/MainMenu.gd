73ee792de25dbb6085f526cb298c1bbea861e15d
Kamil Gombos <gremmlynn@gmail.com>
2023-10-02 13:23:17 -0500

    Added tutorial

--- UI/Scripts/MainMenu.gd
+++ UI/Scripts/MainMenu.gd
@@ -14,8 +14,9 @@ func how_play():
 	get_tree().change_scene_to_file("res://How_To_Play.tscn")
 
 func tutorial():
-	get_tree().change_scene_to_file("res://tutorial.tscn")
+	get_tree().change_scene_to_file("res://arenaTutorial.tscn")
 	MusicPlayer.get_node("GameMusic").play()
+	MusicPlayer.get_node("MenuMusic").stop()
 
 func practice():
 	get_tree().change_scene_to_file("res://arenaPractice.tscn")
