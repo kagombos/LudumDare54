9c9e225b81732ee77f7645d077286b36ef453601
Kamil Gombos <gremmlynn@gmail.com>
2023-10-02 11:30:44 -0500

    initial tutorial

--- SetCursor.gd
+++ SetCursor.gd
@@ -5,3 +5,4 @@ var cursor = load('res://Player/Sprites/Game_Cursor.png')
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_custom_mouse_cursor(cursor)
+
---
+++ Tutorial/TutorialController.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+var tutorialStart = preload("res://Tutorial/Tutorial1.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	add_child(tutorialStart)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
